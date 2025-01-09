e3fe20b625510ff8d7562818ed761a411dda8378
Evangelos Angelakis <Evangelosangel@gmail.com>
2025-01-09 11:13:33 +0000

    Level 4 finished
    Changed js to display the complete password when entered correctly and stop running. An incorrect password message will appear when the password is entered incorrectly. Also added an All in one HTML file to run on phones.

--- Press_Button_To_Win_Game_Level_4.js
+++ Press_Button_To_Win_Game_Level_4.js
@@ -7,7 +7,7 @@ const messages = [
     "Hmm, still not the correct one unfortunately.",
     "It would be best to start from the start!!!",
     "Keep pressing, you'll get it eventually!",
-     "Wrong again! Don't give up! Keep trying.",
+    "Wrong again! Don't give up! Keep trying.",
     "The password is as easy as 1,2,3 but 'odd'!",
     "Not quite. Keep pressing those buttons!",
     "Nope, that's not it. Don't lose hope!",
@@ -18,15 +18,16 @@ const messages = [
     "There is also a '5' in the password, somewhere.",
 ];
 
-
 let password = [1, 3, 5];
 let inputSequence = [];
 let currentIndex = 0;
 let stopExecution = false;
 
-
 function handleButtonPress(buttonNumber) {
     checkPassword(buttonNumber);
+    if (!stopExecution) {
+        displayPressedButtons();
+    }
 }
 
 function checkPassword(buttonNumber) {
@@ -37,26 +38,35 @@ function checkPassword(buttonNumber) {
             stopExecution = true;
             document.getElementById('dText').textContent = "CONGRATULATIONS. YOU WON!!!";
             document.getElementById('dText').style.backgroundColor = "cyan";
+            displayPressedButtons();
             return;
         } else {
-            document.getElementById('dText').innerText = 'The numbers are in series if this helps.';
-            return;
+            document.getElementById('dText').innerText = 'Incorrect Password. Try again!';
         }
         inputSequence = [];
+        resetPressedButtons();
     }
 }
 
+function displayPressedButtons() {
+    const pressedButtonsElement = document.getElementById('dText2');
+    pressedButtonsElement.textContent = `Password: ${inputSequence.join(', ')}`;
+}
+
+function resetPressedButtons() {
+    const pressedButtonsElement = document.getElementById('dText2');
+    pressedButtonsElement.textContent = '';
+    inputSequence = []; 
+}
 
 function cycleMessage() {
     if (stopExecution) return;
-         const dText = document.getElementById('dText');
-         dText.innerText = messages[currentIndex];
-         currentIndex = (currentIndex + 1) % messages.length;
-         document.getElementById('dText').style.backgroundColor = "bisque";
-         document.getElementById('dText').style.color = "black";
+    const dText = document.getElementById('dText');
+    dText.innerText = messages[currentIndex];
+    currentIndex = (currentIndex + 1) % messages.length;
+    document.getElementById('dText').style.backgroundColor = "bisque";
+    document.getElementById('dText').style.color = "black";
 }
-
-
 
 function resetPage() {
     location.reload();
